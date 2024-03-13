0ebbe0a9a73c844007de5e90a201a1f54ade0f95
sitegit <pavlov549@gmail.com>
2024-03-13 20:14:04 +0300

    initial commit

---
+++ app/src/main/java/com/example/customclock/ClockView.kt
@@ -0,0 +1,13 @@
+package com.example.customclock
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClockView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+}
